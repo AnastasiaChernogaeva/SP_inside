c9e3565bc39e95efea367abd0a635033558389a4
AnastasiaChernogaeva <sergeychernogaev1979@gmail.com>
2021-11-16 12:21:04 +0300

    Admin

--- client/src/App.vue
+++ client/src/App.vue
@@ -29,13 +29,13 @@ a{
   text-align: center;
 }
 
-/* body {
+body {
   font-family: Inter, Roboto, Oxygen, Fira Sans, Helvetica Neue, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   font-size: 16px;
   color: #2c3e50;
-  background: #2c3e50;
-} */
+  /* background: #2c3e50; */
+} 
 
 </style>
--- client/src/admin/views/Main.vue
+++ client/src/admin/views/Main.vue
@@ -4,6 +4,24 @@
   <h3>{{activeUser}}</h3>
   <el-button type="danger" @click="logout">Log out</el-button>
 </div>
+<div class="center gr-button">
+  <!-- <el-button-group class="gr-button"> -->
+   <el-button type="info" class="myButton">Clinics</el-button>
+   <el-button type="info" class="myButton">Doctors</el-button>
+   <el-button type="info" class="myButton">Services</el-button>
+   <el-button type="info" class="myButton">Cities</el-button>
+  <!-- </el-button-group> -->
+  
+  <!-- <el-button-group class="gr-button"> -->
+   <el-button type="info" class="myButton">WorkingTime</el-button>
+   <el-button type="info" class="myButton">Pets</el-button>
+   <el-button type="info" class="myButton">Users</el-button>
+   <el-button type="info" class="myButton">Treatments</el-button>
+   <el-button type="info" class="myButton">ShopGoods</el-button>
+
+  <!-- </el-button-group> -->
+
+</div>
 </div>
 </template>
 
@@ -30,5 +48,22 @@ export default {
 .flex-container{
   display: flex;
   justify-content: space-between;
+}
+.myButton{
+    box-shadow: 0 2px 4px #909399, 0 0 6px rgb(0 0 0 / 4%);
+
+    /* box-shadow: 0 2px 4px #d1dbe5, 0 0 6px rgb(0 0 0 / 4%); */
+    --el-button-background-color: #d1dbe5;
+    --el-button-border-color: #d1dbe559;
+}
+.myButton:first-child{
+  margin:0px 0px 0px 10px
+}
+.gr-button{
+  /* text-align: center; */
+  display:flex;
+  flex-direction: column;
+  margin-top:2%;
+  margin-bottom: 2%;
 }
 </style>
