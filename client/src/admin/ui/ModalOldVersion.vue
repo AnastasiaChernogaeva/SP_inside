26ed04d0a6b6eaf8e45e9e53e9cbe9e95abd0fe7
AnastasiaChernogaeva <sergeychernogaev1979@gmail.com>
2021-11-25 10:09:55 +0300

    Save

--- client/src/admin/ui/ModalOldVersion.vue
+++ client/src/admin/ui/ModalOldVersion.vue
@@ -8,8 +8,8 @@
        </teleport> 
 </template>
 <script>
-import AppModal from '../ui/AppModal.vue'
-import FormServices from "../admin/forms/FormServices.vue"
+import AppModal from '../../ui/AppModal.vue'
+import FormServices from "../forms/FormServices.vue"
 // import FormCities from "../forms/FormServices.vue"
 // import FormClinics from "../forms/FormServices.vue"
 // import FormDoctors from "../forms/FormServices.vue"
